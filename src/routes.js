39e7b7e248dabb939ba944f02c7fac43d1ccff60
Guro Walle <guro.terjesdatter.walle@gmail.com>
2021-11-19 13:02:31 +0100

    la til quiz.vue

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 import Dashboard from './views/Dashboard.vue';
+import Quiz from './views/Quiz.vue';
 
 export default [
 	{ name: 'dashboard', path: '/', component: Dashboard },
+	{ name: 'quiz', path: '/quiz', component: Quiz },
+
 ]
---
+++ src/views/Quiz.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Dette er Quiz</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
