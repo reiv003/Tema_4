aaf24c116b3a0789ab22e086a9111ee76897c93d
reiv003 <reiv003@student.kristiania.no>
2021-11-22 15:55:03 +0100

    Fixed front page bug

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,8 @@ import ContactUs from './views/ContactUs.vue';
 import Tabell from './views/Tabell.vue';
 
 export default [
-	{ name: 'dashboard', path: '/dashboard', component: Dashboard },
 	{ name: 'quiz', path: '/quiz', component: Quiz },
 	{ name: 'contact_us', path: '/kontakt-oss', component: ContactUs },
 	{ name: 'tabell', path: '/tabell', component: Tabell },
-
-
+	{ name: 'dashboard', path: '/', component: Dashboard },
 ]
