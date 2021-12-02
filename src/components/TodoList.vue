851220953d3ea407a5fe74bda39fcf793d56388e
Guro Walle <guro.terjesdatter.walle@gmail.com>
2021-12-02 13:56:12 +0100

    Worked on todo-list, added styles to button in reset

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="todo-list">
-    <div class="todo-list__add-task">
-      <button class="todo-list__add-task__svg">
+    <div class="todo-list__header">
+      <button @click="addTask" class="todo-list__add-task__svg">
         <svg
           width="15"
           height="15"
@@ -25,21 +25,40 @@
           />
         </svg>
       </button>
-      <div class="todo-list__add-task__text">Legg til oppgave</div>
+      <div class="todo-list__header__text">{{ title }}</div>
     </div>
 
-    <div class="todo">
-      <button>O</button>
-      <div class="todo__tasks">Have to do this</div>
-      <button>X</button>
+    <div class="todo__items">
+      <button @click="markTaskAsDone">O</button>
+      <div v-for="task in tasks" class="completed__tasks">
+        {{ task.text }}
+      </div>
+      <button @click="removeTask">X</button>
+      <!--
+      <TodoItem
+        v-for="(task, index) in todoTasks"
+        :text="task.text"
+        :done="task.done"
+        :key="task.id"
+      />
+      -->
     </div>
 
     <hr />
-
+    <!--
     <div class="compleded">
       <button>O</button>
       <div class="completed__tasks">Completed this</div>
       <button>X</button>
+    </div>
+    -->
+
+    <div class="compleded">
+      <div class="todo-list__item" v-for="value in completedTasks">
+        <button @click="markTaskAsUndone(index)">O</button>
+        <div>completed</div>
+        <button @click="removeDoneTask(index)">X</button>
+      </div>
     </div>
   </div>
 </template>
@@ -48,16 +67,42 @@
 export default {
   data() {
     return {
-      todoTasks: [
-        { text: "Støvsuge", done: false },
-        { text: "Gjøre lekser", done: false },
-      ],
+      title: "Legg til oppgave",
 
-      completedTasks: [
-        { text: "Gå tur", done: true },
-        { text: "Handle", done: true },
-      ],
+      tasks: [{ text: "Støvsuge", done: false }],
+
+      completedTasks: [{ tekst: "Gå tur", done: true }],
     };
+  },
+
+  computed: {
+    todoTask() {
+      return this.tasks.filter((task) => task.done === false);
+    },
+
+    completedTasks() {
+      return this.tasks.filter((task) => task.done === true);
+    },
+  },
+
+  methods: {
+    addTask(text) {
+      this.tasks.push({ id: this.id(), text: this.id(), done: false });
+    },
+
+    removeTask(id) {
+      const taskIndex = this.tasks.findIndex((task) => task.id === id);
+      this.tasks.splice(taskIndex, 1);
+    },
+
+    markTaskAsDone(id) {
+      const taskIndex = this.tasks.findIndex((task) => task.id === id);
+      this.tasks[taskIndex].done = !this.tasks[taskIndex].done;
+    },
+
+    id() {
+      return Math.random().toString(36).slice(2);
+    },
   },
 };
 </script>
@@ -72,19 +117,13 @@ export default {
   background: palegoldenrod;
 }
 
-.todo-list__add-task {
+.todo-list__header {
   background: violet;
   display: flex;
   flex-direction: row;
 }
 
-.todo-list__add-task__text {
+.todo-list__header__text {
   pointer-events: none;
-}
-
-button {
-  background: none;
-  border: none;
-  font: inherit;
 }
 </style>
