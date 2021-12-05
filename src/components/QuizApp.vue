7d72bfda3bd741f333793f5d66c96cca636b7090
Guro Walle <guro.terjesdatter.walle@gmail.com>
2021-12-05 11:41:01 +0100

    Working on a new quiz, since the last wast to hard coded. Changed the cursor for input in reset

--- src/components/QuizApp.vue
+++ src/components/QuizApp.vue
@@ -1,219 +1,98 @@
 <template>
-  <div>
-    <button @click="startQuiz">start quiz</button>
-  </div>
-  <main id="toggle-quiz" class="quiz-app">
-    <div class="quiz-app__question">{{ currentQuestion }}</div>
-
-    <div class="lol">
-      <button
-        @click="nextQuestion"
-        v-on:click="correct"
-        class="quiz-app__answer"
-      >
-        {{ currentAlternative.a }}
-      </button>
-
-      <button @click="nextQuestion" class="quiz-app__answer">
-        {{ currentAlternative.b }}
-      </button>
-
-      <button class="quiz-app__hidden-answer">
-        {{ currentAlternative.c }}
-
-        <!-- Skriv in "scoren din er ... her?" -->
-      </button>
+  <body class="quiz-body">
+    <div class="quiz-body__title">Kjemisk quiz</div>
+    <div class="quiz-body__box">
+      <div class="box__question">
+        <p>{{ questions[0]["question"] }}</p>
+        <label
+          :for="key"
+          class="box__question__option"
+          v-for="(alternative, key) in questions[0]['alternatives']"
+        >
+          <!--  @change -> når man trykker på et av alternativene skal answered methoden skje
+                :disabled -> gjør at man bare kan velge et av alternativene -->
+          <input
+            class="option-hidden"
+            type="radio"
+            :id="key"
+            :value="key"
+            @change="answered($event)"
+            :disabled="selectedAlternative != ''"
+            :class="{ kanin: selectedAlternative == '' }"
+          />
+          {{ alternative }}
+        </label>
+      </div>
     </div>
-
-    <div class="nav__buttons">
-      <button @click="previousQuestion">Forrigje spørsmål</button>
-      <button @click="nextQuestion">Neste spørsmål</button>
-    </div>
-    <div>Scoren din er {{ score }}</div>
-  </main>
+  </body>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      score: 0,
-      index: 0,
-
-      questions: [
-        "Hvilket grunnstoff var det man tidligere kalte 'surstoff'?",
-        "Hvilket berømt ektepar var de første til å oppdage 'radium' i 1898?",
-        "Hvilket grunnstoff i periodesystemet har det kjemiske symbolet 'W'?",
-        "Hvor mange 'perioder' består det periodiske system av?",
-        "Hva kalles de vertikale kolonnene i det periodiske systemet?",
-        "Quizzen er ferdig",
-      ],
-
-      alternatives: [
-        { a: "Okysgen", b: "Karbondioksid" },
-        { a: "Marie og Pierre Curie", b: "Albert og Elsa Einstein" },
-        { a: "Wolfram", b: "Walterium" },
-        { a: "7 perioder", b: "9 perioder" },
-        { a: "Grupper", b: "Perioder" },
-        { c: "se score" },
-      ],
-
-      /*
+      selectedAlternative: "",
       questions: [
         {
-          question:
-            "Hvilket grunnstoff var det man tidligere kalte 'surstoff'?",
-          alternatives: [
-            { alternative: "Karbondioksid (0)", correct: false },
-            { alternative: "Oksygen (+1)", correct: true },
-          ],
+          question: "Hvem er lederen av EXO?",
+          alternatives: { a: "Onew", b: "Suho", c: "Xiumin", d: "Taeyong" },
+          correctAlternative: "b",
         },
-
         {
-          question:
-            "Hvilket berømt ektepar var de første til å oppdage 'radium' i 1898?",
-          alternatives: [
-            { alternative: "Marie og Pierre Curie (+1)", correct: true },
-            { alternative: "Albert og Elsa Einstein (0)", correct: false },
-          ],
+          question: "Hvem er SHINees maknae?",
+          alternatives: { a: "Taemin", b: "Onew", c: "Yang Yang", d: "Sehun" },
+          correctAlternative: "a",
         },
-
         {
-          question:
-            "Hvilket grunnstoff i periodesystemet har det kjemiske symbolet 'W'?",
-          alternatives: [
-            { alternative: "Wolfram (+1)", correct: true },
-            { alternative: "Walterium (0)", correct: false },
-          ],
+          question: "Hvem er Guros favoritt gruppe?",
+          alternatives: { a: "SHINee", b: "NCT", c: "EXO", d: "Wayv" },
+          correctAlternative: "c",
         },
-
-        {
-          question: "Hvor mange 'perioder' består det periodiske system av?",
-          alternatives: [
-            { alternative: "7 perioder (+1)", correct: true },
-            { alternative: "9 perioder (0)", correct: false },
-          ],
-        },
-
-        {
-          question:
-            "Hva kalles de vertikale kolonnene i det periodiske systemet?",
-          alternatives: [
-            { alternative: "Perioder (0)", correct: false },
-            { alternative: "Grupper (+1)", correct: true },
-          ],
-        },
-      ],*/
+      ],
     };
   },
-
-  computed: {
-    currentQuestion() {
-      return this.questions[this.index];
-    },
-
-    currentAlternative() {
-      return this.alternatives[this.index];
-    },
-  },
-
   methods: {
-    previousQuestion() {
-      this.index =
-        this.index === 0 ? this.questions.length - 1 : this.index - 1;
-      //this.index === this.questions.slice(0, 2);  // Tried making a method that does not loop the array, but stops it after the last object
-    },
-
-    nextQuestion() {
-      this.index =
-        this.index === this.questions.length - 1 ? 0 : this.index + 1;
-    },
-
-    /*
-    scoreCount() {
-      if (this.correct === true) {
-        this.score += 1;
-      }
-    },
-    */
-
-    correct() {
-      this.score += 1;
-    },
-
-    /*
-    vetIkke() {
-      return Math.random() > 0.5 ? "correct" : "wrong";
-    },
-    */
-
-    startquiz() {
-      toggle - quiz;
+    // når man trykker på et alternativ console logger den om man trykker på a,b,c eller d
+    answered(e) {
+      this.selectedAlternative = e.target.value;
+      console.log(this.selectedAlternative);
     },
   },
 };
 </script>
 
-
 <style>
-.quiz-app {
+.quiz-body {
   display: flex;
-  flex-flow: column;
-  align-items: center;
-  width: 50%;
-  height: 70%;
-  margin-bottom: 2em;
+  flex-direction: column;
+  background: burlywood;
+  padding-left: 2em;
 }
 
-.quiz-app__question {
-  background: powderblue;
-  width: 80%;
-  height: 80%;
-  padding: 10%;
-  text-align: center;
+.quiz-body__title {
+  width: 50vw;
+  background: pink;
 }
 
-.quiz-app__answer:hover {
-  background: #ccc;
+.quiz-body__box {
+  width: 50vw;
+  height: 50vh;
+  background: aquamarine;
 }
 
-.lol {
-  width: 60%;
-  height: 40%;
-  background: palegoldenrod;
+.box__question__option {
+  display: block;
+  background: white;
+  margin: 0.5em;
+  border-radius: 20px;
+  padding: 0.06em;
 }
 
-.quiz-app__answer {
-  border: 2px solid black;
-  width: 100%;
-  height: 40%;
-  margin-top: 0.5em;
-  text-align: center;
-  background: none;
-  font-size: 24px;
+.option-hidden {
+  visibility: hidden;
 }
 
-.quiz-app__hidden-answer {
+.kanin {
   background: grey;
-  width: 100%;
-  height: 40%;
-  margin-top: 0.5em;
-  text-align: center;
-  background: none;
-  font-size: 24px;
-}
-
-.nav__buttons {
-  display: flex;
-  flex-direction: row;
-  column-gap: 1em;
-  margin-top: 3em;
-}
-
-.nav__buttons button {
-  width: 70%;
-  border: 2px solid black;
-  background: none;
-  font-size: 20px;
 }
 </style>
